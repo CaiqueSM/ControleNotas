51e63dc127130d66f7054a0fda72365e672d5936
Caíque da Silva Machado <33608459+CaiqueSM@users.noreply.github.com>
2021-10-26 15:26:05 -0300

    Melhorias de legibilidade no script

--- Assets/SQL/notas_db.sql
+++ Assets/SQL/notas_db.sql
@@ -1,53 +1,52 @@
 create DATABASE if not EXISTS nota_fiscal;
 
-use nota_fiscal;
+use notaFiscal;
 
 create table if not EXISTS usuario(
-id_usuario INTEGER PRIMARY KEY AUTO_INCREMENT,
-nome_usuario VARCHAR(80) NOT NULL UNIQUE,
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+nome  VARCHAR(80) NOT NULL UNIQUE,
 senha VARCHAR(256) NOT NULL
 );
 
 create table if not EXISTS notas(
-chave_acesso CHAR(44) NOT NULL PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+chave_acesso VARCHAR(44) NOT NULL,
 codigo INTEGER NOT NULL,
 descricao VARCHAR(140) NOT NULL,
 valor DECIMAL,
-id_usuario INTEGER REFERENCES usuario 
+id INTEGER REFERENCES usuario 
 );
 
 create table if not EXISTS fornecedor(
-CNPJ_CPF CHAR(14) NOT NULL PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+CNPJ_CPF VARCHAR(14) NOT NULL,
 nome_fornecedor VARCHAR(80)
 );
 
 create table if not EXISTS cliente(
-CNPJ_CPF CHAR(14) NOT NULL PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+CNPJ_CPF VARCHAR(14) NOT NULL PRIMARY KEY,
 nome_cliente VARCHAR(80)
 );
 
 create table if not EXISTS contato(
-id_contato integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
 CEP CHAR(8) NOT NULL,
-nome_rua VARCHAR(80),
+nomeRua VARCHAR(80) NOT NULL,
 bairro VARCHAR(32),
 cidade VARCHAR(32),
 numero VARCHAR(9) NOT NULL
+complemento VARCHAR(64)
 );
 
 create table if not EXISTS email(
-endereco_email VARCHAR(64) NOT NULL PRIMARY KEY,
-id_contato integer NULL REFERENCES contato
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+enderecoEmail VARCHAR(64) NOT NULL,
+id integer NULL REFERENCES contato
 );
 
 create table if not EXISTS telefone(
-numero_telefone VARCHAR(11) NOT NULL PRIMARY KEY,
-id_contato integer NOT NULL REFERENCES contato
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+numerotelefone VARCHAR(11) NOT NULL,
+id integer NOT NULL REFERENCES contato
 );
-
-use nota_fiscal;
-insert into usuario(nome_usuario, senha) values('caique2', '12345');
-select * from usuario;
-drop table usuario;
-
-
