9232a3e8e48fd4af00ac06c3ba5480683df4a654
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-27 18:31:09 -0300

    Add os formulários

--- ControleNotas.dpr
+++ ControleNotas.dpr
@@ -10,14 +10,15 @@ uses
   untconexao in 'Dao\untconexao.pas',
   untlogindao in 'Dao\untlogindao.pas',
   untusuariomodel in 'Model\untusuariomodel.pas',
-  consulta_notas in 'Views\consulta_notas.pas',
-  controle_notas in 'Views\controle_notas.pas',
-  untmenu in 'Views\untmenu.pas';
+  consulta_notas in 'Views\consulta_notas.pas' {frmNotas},
+  controle_notas in 'Views\controle_notas.pas' {frmLogin},
+  untmenu in 'Views\untmenu.pas' {frmMenu};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMenu, frmMenu);
   Application.Run;
 end.
