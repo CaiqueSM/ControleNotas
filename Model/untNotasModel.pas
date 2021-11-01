eeca807440f9a5bda2190c9716c955020cd19a40
Caíque da Silva Machado <caique9machado@gmail.com>
2021-11-01 14:30:14 -0300

    Update untNotasModel.pas

--- Model/untNotasModel.pas
+++ Model/untNotasModel.pas
@@ -3,11 +3,7 @@ unit untNotasModel;
 interface
 
 uses
-<<<<<<< Updated upstream
-Classes, SysUtils, UntUsuarioModel;
-=======
   Classes, SysUtils, untPessoaModel;
->>>>>>> Stashed changes
 
 type
 
@@ -18,19 +14,6 @@ type
     FCodigo: integer;
     FDescricao: string;
     FValor: real;
-<<<<<<< Updated upstream
-    FUsuario: TUsuarioModel;
-    procedure SetChaveAcesso(const Value: String);
-    procedure SetDescricao(const Value: String);
-    procedure SetUsuario(const Value: TUsuarioModel);
-  public
-    property Id: Integer read FId write FId;
-    property ChaveAcesso: String read FChaveAcesso write SetChaveAcesso;
-    property Codigo: Integer read FCodigo write FCodigo;
-    property Descricao: String read FDescricao write SetDescricao;
-    property Valor: real read FValor write FValor;
-    property Usuario: TUsuarioModel read FUsuario write SetUsuario;
-=======
     FCliente: TPessoaModel;
     FFornecedor: TPessoaModel;
     procedure SetChaveAcesso(AValue: string);
@@ -46,32 +29,12 @@ type
     property Valor: real read FValor write FValor;
     property Cliente: TPessoaModel read FCliente write FCliente;
     property Fornecedor: TPessoaModel read FFornecedor write FFornecedor;
->>>>>>> Stashed changes
   end;
 
 implementation
 
 { TNotas }
 
-<<<<<<< Updated upstream
-procedure TNotas.SetChaveAcesso(const Value: String);
-begin
-  FChaveAcesso := Trim(Value);
-end;
-
-procedure TNotas.SetDescricao(const Value: String);
-begin
-  FDescricao := Trim(Value);
-end;
-
-procedure TNotas.SetUsuario(const Value: TUsuarioModel);
-begin
-   If (Usuario <> Value) and (Value <> Nil) then
-      Begin
-         Usuario.Free;
-         Usuario := Value;
-      End;
-=======
 procedure TNotas.SetChaveAcesso(AValue: string);
 begin
   FChaveAcesso := Trim(AValue);
@@ -84,7 +47,7 @@ end;
 
 procedure TNotas.SetCodigo(AValue: integer);
 begin
-  FCodigo := AValue;
+  FCodigo:= AValue;
 end;
 
 procedure TNotas.SetDescricao(AValue: string);
@@ -94,13 +57,12 @@ end;
 
 procedure TNotas.SetFornecedor(AValue: TPessoaModel);
 begin
-  FFornecedor := AValue;
+
 end;
 
 procedure TNotas.SetValor(AValue: real);
 begin
-  FValor := AValue;
->>>>>>> Stashed changes
+
 end;
 
 end.
