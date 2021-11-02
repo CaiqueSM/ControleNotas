cf11aea431f3d6d48f732dda6ccc88f065baf439
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-11-02 12:55:18 -0300

    Criando uma classe helper para o tform

---
+++ Helper/UntFormHelper.pas
@@ -0,0 +1,25 @@
+unit UntFormHelper;
+
+interface
+
+uses
+  Forms;
+
+type
+  TFormHelper = class helper for TForm
+    procedure TabOrderNext();
+  end;
+
+implementation
+
+uses
+  Winapi.Messages;
+
+{ TFormHelper }
+
+procedure TFormHelper.TabOrderNext;
+begin
+   Perform(wm_NextDlgCtl,0,0);
+end;
+
+end.
