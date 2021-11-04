284936662385fafe45bfb0acd6c6b9af2a8635ad
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-11-03 21:32:52 -0300

    Removendo o formulário da iniciação do sistema

--- ControleNotas.dpr
+++ ControleNotas.dpr
@@ -46,7 +46,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmMenu, frmMenu);
-  Application.CreateForm(TfrmCliente, frmCliente);
   Application.Run;
 
 end.
