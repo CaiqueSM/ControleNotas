d73f1da9a5202e9aa070bc2d3f5de9e3b9ebf809
Caíque da Silva Machado <caique9machado@gmail.com>
2021-11-05 08:56:58 -0300

    Adição de restrições de integridade para notas e contatos.
    Adição da restrição "ON DELETE CASCADE".

--- Assets/SQL/Initial.sql
+++ Assets/SQL/Initial.sql
@@ -8,15 +8,6 @@ nome  VARCHAR(80) NOT NULL,
 senha VARCHAR(256) NOT NULL
 );
 
-create table if not EXISTS notas(
-id INTEGER NOT NULL PRIMARY KEY,
-chaveacesso VARCHAR(44) NOT NULL,
-codigo INTEGER NOT NULL,
-descricao VARCHAR(140) NOT NULL,
-valor DECIMAL,
-idusuario INTEGER REFERENCES usuario 
-);
-
 create table if not EXISTS fornecedor(
 id INTEGER NOT NULL PRIMARY KEY,
 CNPJ VARCHAR(14),
@@ -31,6 +22,17 @@ CPF VARCHAR(11),
 nome VARCHAR(80) NOT NULL
 );
 
+create table if not EXISTS notas(
+id INTEGER NOT NULL PRIMARY KEY,
+chaveacesso VARCHAR(44) NOT NULL,
+codigo INTEGER NOT NULL,
+descricao VARCHAR(140) NOT NULL,
+valor DECIMAL,
+idusuario INTEGER REFERENCES usuario ON DELETE cascade,
+idcliente INTEGER REFERENCES cliente ON DELETE cascade,
+idfornecedor INTEGER REFERENCES fornecedor ON DELETE cascade
+);
+
 create table if not EXISTS contato(
 id integer PRIMARY KEY AUTO_INCREMENT,
 CEP VARCHAR(9) NOT NULL,
@@ -38,19 +40,21 @@ nomeRua VARCHAR(80) NOT NULL,
 bairro VARCHAR(32),
 cidade VARCHAR(32),
 numero VARCHAR(9) NOT NULL,
-complemento VARCHAR(64)
+complemento VARCHAR(64),
+idcliente INTEGER REFERENCES cliente ON DELETE cascade,
+idfornecedor INTEGER REFERENCES fornecedor ON DELETE cascade 
 );
 
 create table if not EXISTS email(
 id INTEGER PRIMARY KEY AUTO_INCREMENT,
 email VARCHAR(64) NOT NULL,
-idcontato integer NULL REFERENCES contato
+idcontato integer NOT NULL REFERENCES contato ON DELETE cascade
 );
 
 create table if not EXISTS telefone(
 id INTEGER PRIMARY KEY AUTO_INCREMENT,
 telefone VARCHAR(14) NOT NULL,
-idcontato integer NOT NULL REFERENCES contato
+idcontato integer NOT NULL REFERENCES contato ON DELETE cascade
 );
 
 use notafiscal;
--- Dao/UntContatoDao.pas
+++ Dao/UntContatoDao.pas
@@ -2,6 +2,21 @@ unit UntContatoDao;
 
 interface
 
+uses
+
+  untbasedao, System.Generics.Collections, UntContatoModel, System.Classes;
+
+type
+
+  TContatoDao = class(TBaseDao)
+  public
+    function ListarContatos(): TObjectList<TContatoModel>;
+    function Consultar(ACodigo: integer): TContatoModel;
+    procedure Criar(ACliente: TContatoModel);
+    procedure Alterar(ACliente: TContatoModel);
+    procedure Excluir(AId: Integer);
+  end;
+
 implementation
 
 end.
