4bddab02eddc16f25e3eb32a30328f36205d8636
Caíque da Silva Machado <caique9machado@gmail.com>
2021-11-06 18:26:13 -0300

    Criação da classe de conexão ao banco de dados para a classe Email.

--- Dao/UntEmailDao.pas
+++ Dao/UntEmailDao.pas
@@ -3,12 +3,17 @@ unit UntEmailDao;
 interface
 
 uses
-  untBaseDao, System.Generics.Collections, UntContatoModel, System.Classes;
+  untBaseDao, System.Generics.Collections, UntEmailModel, System.Classes;
 
 type
 
   TEmailDao = class(TBaseDao)
-
+  public
+    function ListarEmails(): TObjectList<TEmailModel>;
+    function Consultar(ACodigo: integer): TEmailModel;
+    procedure Criar(AContato: TEmailModel);
+    procedure Alterar(AContato: TEmailModel);
+    procedure Excluir(ACodigo: integer);
   end;
 
 implementation
