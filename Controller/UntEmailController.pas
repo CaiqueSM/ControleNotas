901c6ff7beb6116d74dfceac3b3ba1ef0cb3e0a1
Caíque da Silva Machado <caique9machado@gmail.com>
2021-11-07 11:33:10 -0300

    Implementação da classe de controle para os objetos de email.

--- Controller/UntEmailController.pas
+++ Controller/UntEmailController.pas
@@ -37,23 +37,24 @@ end;
 
 constructor TEmailController.Create;
 begin
-
+  inherited;
+  FDao := TEmailDao.Create(Conexao);
 end;
 
 procedure TEmailController.Criar(AEmail: TEmailModel);
 begin
-
+  FDao.Criar(AEmail);
 end;
 
 destructor TEmailController.Destroy;
 begin
-
+   FDao.Free;
   inherited;
 end;
 
 procedure TEmailController.Excluir(ACodigo: integer);
 begin
-
+  FDao.Excluir(ACodigo);
 end;
 
 end.
