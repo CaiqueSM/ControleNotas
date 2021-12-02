2615a6174535a986dfa90fd4c200047de75f09e7
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-02 19:16:20 -0300

    Alteração nos campos da tabela notas
    - Adição do campo  emissão.
    - mudança nas restrições das colunas descrição e campo.

--- Assets/SQL/Initial.sql
+++ Assets/SQL/Initial.sql
@@ -26,13 +26,13 @@ PRIMARY KEY(id)
 );
 
 create table if not EXISTS notas(
-id INTEGER NOT NULL,
+id INTEGER AUTO_INCREMENT NOT NULL,
 idUsuario integer,
 idCliente integer,
 idFornecedor integer,
 chaveacesso VARCHAR(44) NOT NULL,
 controle INTEGER NOT NULL,
-descricao VARCHAR(140) NOT NULL,
+descricao VARCHAR(500) NOT NULL,
 emissao date NOT NUll,
 valor DECIMAL,
 PRIMARY KEY(id),
