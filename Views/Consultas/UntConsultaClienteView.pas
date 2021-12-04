5e0f2516b8c1b21e6dcfbdab048e5beeb1bf0f6a
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-04 17:09:43 -0300

    Criação do formulário para exibir o resultado da consulta por nome.

---
+++ Views/Consultas/UntConsultaClienteView.pas
@@ -0,0 +1,88 @@
+unit UntConsultaClienteView;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids, Vcl.DBGrids, untenvironment,
+  UntConsultarClienteController;
+
+type
+  TFrmConsultaClienteView = class(TForm)
+    gridClienteView: TStringGrid;
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure gridClienteViewKeyPress(Sender: TObject; var Key: Char);
+    procedure gridClienteViewDblClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    FResultado: String;
+    FController: TConsultaClienteController;
+    procedure carregarDados();
+  public
+    property Resultado: String Read FResultado;
+  end;
+
+var
+  FrmConsultaClienteView: TFrmConsultaClienteView;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFrmConsultaClienteView.FormClose(Sender: TObject;
+  var Action: TCloseAction);
+begin
+  FController.Free;
+  Action := caFree;
+end;
+
+procedure TFrmConsultaClienteView.FormCreate(Sender: TObject);
+begin
+  FController := TConsultaClienteController.Create();
+end;
+
+procedure TFrmConsultaClienteView.FormShow(Sender: TObject);
+begin
+  carregarDados();
+end;
+
+procedure TFrmConsultaClienteView.gridClienteViewDblClick(Sender: TObject);
+var
+  linha: Integer;
+begin
+  linha := gridClienteView.Row;
+  FResultado := gridClienteView.Rows[linha].Text;
+  Close();
+end;
+
+procedure TFrmConsultaClienteView.gridClienteViewKeyPress(Sender: TObject;
+  var Key: Char);
+begin
+  If (Key = BotaoEnter) Then
+    gridClienteViewDblClick(Sender);
+end;
+
+procedure TFrmConsultaClienteView.carregarDados;
+var
+  nomesClienteView: TStringList;
+  nome: String;
+  coluna, linha: Integer;
+begin
+  coluna := 0;
+  nomesClienteView := FController.ListarNomesClientes();
+  Try
+    linha := 0;
+    for nome in nomesClienteView do
+    begin
+      gridClienteView.Cells[coluna, linha] := nome;
+      linha := linha + 1;
+      gridClienteView.RowCount := linha;
+    end;
+  Finally
+    nomesClienteView.Free;
+  End;
+end;
+
+end.
