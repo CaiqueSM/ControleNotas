6b083254dc3863a192472e9fb93a9e0fda1b2ef6
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-06 05:22:42 -0300

    Criação de enumerador para classe relatório.

---
+++ Model/Enum/UntRelatorioEnum.pas
@@ -0,0 +1,12 @@
+unit UntRelatorioEnum;
+
+interface
+
+type
+
+TTipoRelatorio = (actCliente, actFornecedor, actNotas);
+TOrdemRelatorio = (actAfabetico, actFrequencia, actValores);
+
+implementation
+
+end.
