5d0e264fa4f4861153bde54aa0f4507317211f6a
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-09 19:12:49 -0300

    Implementação da classe de acesso ao BD para a classe relatório.

--- Dao/UntRelatorioDao.pas
+++ Dao/UntRelatorioDao.pas
@@ -4,19 +4,57 @@ interface
 
 uses
   untBaseDao, System.Generics.Collections, System.Classes,
-  UntClienteModel, UntFornecedorModel, UntNotasModel;
+  UntClienteDao, UntFornecedorDao, UntNotasDao, UntRelatorioModel,
+  UntClienteModel, UntFornecedorModel, UntNotasModel, UntRelatorioEnum;
 
 type
 
-  TEmailDao = class(TBaseDao)
+  TRelatorioDao = class(TBaseDao)
+  private
+    FCliente: TClienteDao;
+    FFornecedor: TFornecedorDao;
+    FNotas: TNotasDao;
   public
-    function Consultar(AIdContato: integer): TObjectList<TEmailModel>;
-    function Criar(AEmail: TEmailModel): Boolean;
-    function Alterar(AEmail: TEmailModel): Boolean;
-    function Excluir(AIdContato: integer): Boolean;
+    constructor Create();
+    destructor Destroy();override;
+    function ConsultarCliente(ASQL: string): TObjectList<TClienteModel>;
+    function ConsultarFornecedor(ASQL: string): TObjectList<TFornecedorModel>;
+    function ConsultarNotas(ASQL: string): TObjectList<TNotasModel>;
   end;
 
-
 implementation
+
+{ TRelatorioDao }
+
+function TRelatorioDao.ConsultarCliente(ASQL: string)
+  : TObjectList<TClienteModel>;
+begin
+  Result := FCliente.ListarClientes(ASQL);
+end;
+
+function TRelatorioDao.ConsultarFornecedor(ASQL: string)
+  : TObjectList<TFornecedorModel>;
+begin
+  Result := FFornecedor.ListarFornecedores(ASQL);
+end;
+
+function TRelatorioDao.ConsultarNotas(ASQL: string): TObjectList<TNotasModel>;
+begin
+  Result := FNotas.ListarNotas(ASQL);
+end;
+
+constructor TRelatorioDao.Create;
+begin
+  FCliente := TClienteDao.Create(Conexao);
+  FFornecedor := TFornecedorDao.Create(Conexao);
+  FNotas := TNotasDao.Create(Conexao);
+end;
+
+destructor TRelatorioDao.Destroy;
+begin
+  FCliente.Free;
+  FFornecedor.Free;
+  FNotas.Free;
+end;
 
 end.
