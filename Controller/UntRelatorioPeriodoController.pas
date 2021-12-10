adee973c571482271fbe9a7cf41e6c250973bcf2
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-10 09:03:11 -0300

    Implementação da classe de controle para relatório.

--- Controller/UntRelatorioPeriodoController.pas
+++ Controller/UntRelatorioPeriodoController.pas
@@ -5,24 +5,69 @@ interface
 uses
 
   Classes, SysUtils, UntBaseController, UntRelatorioDao, UntRelatorioModel,
-  System.Generics.Collections;
+  UntRelatorioEnum, System.Generics.Collections;
 
 type
 
   TRelatorioPeriodoController = class(TBaseController)
+  private
+     FRelatorioDao: TRelatorioDao;
   public
     function CriarRelatorioPeriodo(ARelatorio: TRelatorioModel): boolean;
-
+    constructor Create();
+    destructor Destroy();override;
   end;
 
 implementation
 
 { TRelatorioPeriodoController }
 
+constructor TRelatorioPeriodoController.Create;
+begin
+   inherited;
+   FRelatorioDao:= TRelatorioDao.Create(Conexao);
+end;
+
 function TRelatorioPeriodoController.CriarRelatorioPeriodo(
   ARelatorio: TRelatorioModel): boolean;
+ var
+   sql, tabela: string;
+   id: integer;
+   dataInicio, dataTermino: TDate;
 begin
 
+case ARelatorio.Tipo of
+
+actNotas:
+begin
+  tabela:= 'notas';
+  case ARelatorio.Ordem of
+    actAlfabetica:
+    begin
+
+    end;
+    actFrequencia:
+    begin
+
+    end;
+    actValores:
+    begin
+
+    end;
+  end;
+end;
+
+end;
+sql := 'select * from notas(tabela) where idUsuario = 1(id)' +
+'and emissao between' + '2021-12-01(DI) and 2021-12-07(DF)';
+
+
+end;
+
+destructor TRelatorioPeriodoController.Destroy;
+begin
+
+  inherited;
 end;
 
 end.
