bfcfe1ef039ad8b11dade83926523b2f5642eba4
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-10 09:09:26 -0300

    Adição de novas propriedades a classe relatório.

--- Model/UntRelatorioModel.pas
+++ Model/UntRelatorioModel.pas
@@ -10,18 +10,46 @@ type
 TRelatorioModel = class
 private
   Fid: integer;
+  FIdUsuario: integer;
   FTipo: TTipoRelatorio;
   FOrdem: TOrdemRelatorio;
   FDataInicio: Tdate;
   FDataTermino: TDate;
 public
-  property ID: integer read Fid write Fid;
+  property Id: integer read Fid write Fid;
+  property IdUsuario: integer read FidUsuario write FidUsuario;
   property Tipo: TTipoRelatorio read FTipo write FTipo;
   property Ordem: TOrdemRelatorio read FOrdem write Fordem;
   property DataInicio: TDate read FDataInicio write FDataInicio;
   property DataTermino: TDate read FDataTermino write FDataTermino;
+  constructor Create();
+  destructor Destroy();override;
+  function Exibir(): boolean;
+  function Exportar(): boolean;
 end;
 
 implementation
+
+{ TRelatorioModel }
+
+constructor TRelatorioModel.Create;
+begin
+
+end;
+
+destructor TRelatorioModel.Destroy;
+begin
+
+end;
+
+function TRelatorioModel.Exibir: boolean;
+begin
+
+end;
+
+function TRelatorioModel.Exportar: boolean;
+begin
+
+end;
 
 end.
