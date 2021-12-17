a8d423a0a40c0a3cd88188dde982f79d443beb48
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-17 05:03:44 -0300

    Atualização das funções que geram os relatórios.

--- Controller/UntRelatorioPeriodoController.pas
+++ Controller/UntRelatorioPeriodoController.pas
@@ -15,12 +15,9 @@ type
     FRelatorioDao: TRelatorioDao;
     FValidarData: TValidarData;
   public
-    function CriarRelatorioNotas(ARelatorio: TRelatorioModel)
-      : TZQuery;
-    function CriarRelatorioCliente(ARelatorio: TRelatorioModel)
-      : TZQuery;
-    function CriarRelatorioFornecedor(ARelatorio: TRelatorioModel)
-      : TZQuery;
+    function CriarRelatorioNotas(ARelatorio: TRelatorioModel): TZQuery;
+    function CriarRelatorioCliente(ARelatorio: TRelatorioModel): TZQuery;
+    function CriarRelatorioFornecedor(ARelatorio: TRelatorioModel): TZQuery;
     function ValidarData(AData: TDate): boolean;
     constructor Create();
     destructor Destroy(); override;
@@ -41,12 +38,10 @@ function TRelatorioPeriodoController.CriarRelatorioCliente
   (ARelatorio: TRelatorioModel): TZQuery;
 begin
 
-  ARelatorio.Tipo := 'cliente as c';
-
   if ARelatorio.ordem = 'Alfabética' then
     ARelatorio.ordem := ' order by nome asc'
   else if ARelatorio.ordem = 'Frequência' then
-    ARelatorio.ordem := ' order by count(nome) desc'
+    ARelatorio.ordem := ' group by nome'
   else
     ARelatorio.ordem := ' order by valor desc';
 
@@ -55,41 +50,27 @@ end;
 
 function TRelatorioPeriodoController.CriarRelatorioFornecedor
   (ARelatorio: TRelatorioModel): TZQuery;
-var
-  sql, tabela, ordem: string;
 begin
 
-  tabela := 'fornecedor as f';
-
   if ARelatorio.ordem = 'Alfabética' then
-    ordem := ' order by nome asc'
+    ARelatorio.ordem := 'order by nome asc'
   else if ARelatorio.ordem = 'Frequência' then
-    ordem := ' order by count(nome) desc'
+    ARelatorio.ordem := 'group by nome'
   else
-    ordem := ' order by valor desc';
-
-  sql := 'select id, cpf, cnpj, nome, count(nome) from ' + tabela +
-    ', notas as n where f.id = n.idFornecedor and n.idUsuario = ' +
-    intTOstr(ARelatorio.IdUsuario) + ' and emissao between ' +
-    dateTOstr(ARelatorio.DataInicio) + ' and ' +
-    dateTOstr(ARelatorio.DataTermino) + ordem;
+    ARelatorio.ordem := 'order by valor desc';
 
   Result := FRelatorioDao.ConsultarFornecedor(ARelatorio);
 end;
 
 function TRelatorioPeriodoController.CriarRelatorioNotas
   (ARelatorio: TRelatorioModel): TZQuery;
-var
-  sql, tabela, ordem: string;
 begin
-  tabela := 'notas';
-
   if ARelatorio.ordem = 'Alfabética' then
-    ordem := ' order by id asc'
+    ARelatorio.ordem := 'order by id asc'
   else if ARelatorio.ordem = 'Frequêcia' then
-    ordem := ' order by count(emissao) desc'
+    ARelatorio.ordem := 'group by valor'
   else
-    ordem := ' order by valor desc';
+    ARelatorio.ordem := 'order by valor desc';
 
   Result := FRelatorioDao.ConsultarNotas(ARelatorio);
 end;
