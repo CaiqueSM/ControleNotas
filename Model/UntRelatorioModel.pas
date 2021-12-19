90e45f5e8e6cfa78b7f0d32f3c720302d13102c9
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-18 21:24:56 -0300

    Desmembramento da classe RelatorioModel

--- Model/UntRelatorioModel.pas
+++ Model/UntRelatorioModel.pas
@@ -12,14 +12,10 @@ private
   FIdUsuario: integer;
   FTipo: string;
   FOrdem: string;
-  FDataInicio: Tdate;
-  FDataTermino: TDate;
 public
   property IdUsuario: integer read FidUsuario write FidUsuario;
   property Tipo: string read FTipo write FTipo;
   property Ordem: string read FOrdem write Fordem;
-  property DataInicio: TDate read FDataInicio write FDataInicio;
-  property DataTermino: TDate read FDataTermino write FDataTermino;
 end;
 
 implementation
