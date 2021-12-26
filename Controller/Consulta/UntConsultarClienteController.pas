3354464432b4951e39f73bba6740402937b6aaf2
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-26 08:59:42 -0300

    Adição do atributo ListaID a classe ConsultaClienteController.
    ListaID retorna uma  lista contendo o ID de todos os clientes na ordem em que são encontrados.

--- Controller/Consulta/UntConsultarClienteController.pas
+++ Controller/Consulta/UntConsultarClienteController.pas
@@ -6,14 +6,18 @@ uses UntBaseController, System.Generics.Collections, UntClienteModel,
   UntClienteDao, System.Classes, SysUtils;
 
 type
+
+  ArrayInteger = array of integer;
+
   TConsultaClienteController = class(TBaseController)
   private
     FDao: TClienteDao;
+    ListaIDClientes: ArrayInteger;
   public
     constructor Create(); reintroduce;
     destructor Destroy(); override;
-
     function ListarNomesClientes(): TStringList;
+    property ListaID: ArrayInteger read ListaIDClientes;
   end;
 
 implementation
@@ -36,17 +40,24 @@ function TConsultaClienteController.ListarNomesClientes: TStringList;
 var
   Clientes: TObjectList<TClienteModel>;
   Cliente: TClienteModel;
+  indice: integer;
 begin
   Result := TStringList.Create();
   Clientes := FDao.ListarClientes();
   if Clientes = nil then
   begin
-    Result:= nil;
+    Result := nil;
     exit();
   end;
+  SetLength(ListaIDClientes, Clientes.Count);
+  indice := 0;
   Try
     For Cliente In Clientes Do
+    begin
       Result.Add(Cliente.nome);
+      ListaIDClientes[indice] := Cliente.Id;
+      inc(indice);
+    end;
   Finally
     Clientes.Free;
   End;
