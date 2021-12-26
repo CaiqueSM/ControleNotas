43d1368ce184aa86e676b45637cf4430937b7b03
Caíque da Silva Machado <caique9machado@gmail.com>
2021-12-26 09:02:35 -0300

    Mudança no tipo de dado retornado por ConsultarClientes.
    - O tipo passa de String para o TResultado.

--- Controller/Consulta/UntConsultarCliente.pas
+++ Controller/Consulta/UntConsultarCliente.pas
@@ -7,14 +7,14 @@ uses UntConsultaClienteView;
 type
   TConsultaCliente = class
   public
-    class function ConsultarClientes(): String;
+    class function ConsultarClientes(): TResultado;
   end;
 
 implementation
 
 { TConsultaClienteView }
 
-class function TConsultaCliente.ConsultarClientes: String;
+class function TConsultaCliente.ConsultarClientes: TResultado;
 var
   formulario: TFrmConsultaClienteView;
 begin
